b6ef77f33efd9cfb4375dce706ed93983799cab2
suNoeul(CSE_edu_cluster) <rlatnsgh0708@postech.ac.kr>
2025-05-25 01:37:10 +0900

    Remove basicsr and basicsr_RF from Git tracking and update .gitignore

--- NAFNet/basicsr/version.py
+++
@@ -1,5 +0,0 @@
-# GENERATED VERSION FILE
-# TIME: Fri May  9 08:39:02 2025
-__version__ = '1.2.0+2b4af71'
-short_version = '1.2.0'
-version_info = (1, 2, 0)
--- NAFNet/basicsr_RF/version.py
+++
@@ -1,5 +0,0 @@
-# GENERATED VERSION FILE
-# TIME: Fri Apr 26 02:27:59 2024
-__version__ = '1.2.0+50cb149'
-short_version = '1.2.0'
-version_info = (1, 2, 0)
